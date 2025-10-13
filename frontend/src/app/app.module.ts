b3fe310ef621620fe6b4b876d6e8d467a657830d
Alejandro Abadía <ajabadia@gmail.com>
2025-10-14 00:41:25 +0200

    Preparando para la actualización de Angular

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,25 +4,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 // ✅ 1. Importa el módulo de animaciones del navegador
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './modules/shared/shared.module';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    // ✅ 2. Añade BrowserAnimationsModule a la lista de imports
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    SharedModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        // ✅ 2. Añade BrowserAnimationsModule a la lista de imports
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        SharedModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
--- frontend/src/app/modules/shared/models/ui-icon.module.ts
+++ frontend/src/app/modules/shared/models/ui-icon.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiIconComponent } from '../components/ui-icon/ui-icon.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
-@NgModule({
-  declarations: [UiIconComponent],
-  imports: [CommonModule, HttpClientModule],
-  exports: [UiIconComponent],
-})
+@NgModule({ declarations: [UiIconComponent],
+    exports: [UiIconComponent], imports: [CommonModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class UiIconModule { }
 
